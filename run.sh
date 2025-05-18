98358990399b0ae67e3f7f1fc6a367dfb0c49c48
Jvinicius06 <30390323+Jvinicius06@users.noreply.github.com>
2025-05-18 13:43:29 -0300

    torna o run.sh executavel

--- run.sh
+++ run.sh
@@ -15,5 +15,7 @@ fi
 # Gera o server.cfg usando envsubst
 envsubst < /fx-data/scripts-base/server.template.cfg > /fx-data/scripts-base/server.cfg
 
+chmod +x /opt/fxserver/run.sh
+
 # Inicia o FXServer
 sh /opt/fxserver/run.sh
