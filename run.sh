ed7c135eeb918db873ed39d34177d62580df6728
Jvinicius06 <30390323+Jvinicius06@users.noreply.github.com>
2025-05-18 13:04:18 -0300

    correcao do path

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Espera o bind mount ficar disponível
-while [ ! -f /fx-data/server.template.cfg ]; do
+while [ ! -f /fx-data/script-base/server.template.cfg ]; do
   echo "Aguardando bind /fx-data..."
   sleep 1
 done
 
 # Continua normalmente
-envsubst < /fx-data/server.template.cfg > /fx-data/server.cfg
+envsubst < /fx-data/script-base/server.template.cfg > /fx-data/script-base/server.cfg
 /opt/fxserver/run.sh
