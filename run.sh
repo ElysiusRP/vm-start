3e0bc78eca57d750ae64a162aa0bc65326064597
Jvinicius06 <30390323+Jvinicius06@users.noreply.github.com>
2025-05-18 13:26:14 -0300

    correcao de path

--- run.sh
+++ run.sh
@@ -7,13 +7,13 @@ while [ ! -d /fx-data ]; do
 done
 
 # Verifica se o template existe
-if [ ! -f /fx-data/script-base/server.template.cfg ]; then
-  echo "❌ Template não encontrado: /fx-data/script-base/server.template.cfg"
+if [ ! -f /fx-data/scripts-base/server.template.cfg ]; then
+  echo "❌ Template não encontrado: /fx-data/scripts-base/server.template.cfg"
   exit 1
 fi
 
 # Gera o server.cfg usando envsubst
-envsubst < /fx-data/script-base/server.template.cfg > /fx-data/script-base/server.cfg
+envsubst < /fx-data/scripts-base/server.template.cfg > /fx-data/scripts-base/server.cfg
 
 # Inicia o FXServer
 exec /opt/fxserver/run.sh
